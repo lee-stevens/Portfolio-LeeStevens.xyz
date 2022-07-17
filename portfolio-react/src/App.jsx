380775c4ac9a4cd86dffde6e017c17df211bb419
leesprojects <leepeterstevens@gmail.com>
2022-07-17 18:28:53 +0100

    Content add and basic styling

--- portfolio-react/src/App.jsx
+++ portfolio-react/src/App.jsx
@@ -5,26 +5,129 @@ function App() {
   return (
     <div className="App">
 
-      <section>
-        <nav class="navBar">
-          <a href="https://www.linkedin.com/in/lee-stevens-dev/">Linked In</a>
-          <a href="https://github.com/leesprojects">GitHub</a>
-          <a href="https://leetcode.com/Zaeroso/">LeetCode</a>
-        </nav>
-      </section>
-
       <main>
-          <section>
-            <img class="logo" src="images/icon - ls.jpg"></img>
-            <h1>Lee Stevens | Portfolio </h1>
-          </section>
+        <section id="navBar">
+          <nav>
+            <ul>
+              <li>
+                <a href="https://www.linkedin.com/in/lee-stevens-dev/">LinkedIn</a>
+              </li>
+              <li>
+                <a href="https://github.com/leesprojects">GitHub</a>
+              </li>
+              <li>
+                <a href="https://leetcode.com/Zaeroso/">Leetcode</a>
+              </li>
+            </ul>
+          </nav>
+        </section>
 
-          <section>
-            <button>Press me</button>
-          </section>
+        <section class="titleSection">
+          <div>
+            <video autoplay muted>
+              <source src="videos/Video - Test A.mp4"></source>
+            </video>
+          </div>
+          <div>
+            <div>
+              <img class="logo" src="images/icon - ls.jpg"></img>
+            </div>
+            <div>
+              <h1>Lee Stevens | Portfolio </h1>
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <div class="sectionHeader">
+            <h1>About Me</h1>
+          </div>
+          <div class="sectionContent">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla rhoncus convallis consectetur. Cras lorem nibh, tincidunt ut molestie ac, tristique nec leo. Suspendisse non laoreet nisi, eu ultrices augue. Morbi vitae orci sagittis, ullamcorper massa sed, commodo lectus. Suspendisse potenti. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer sit amet justo vulputate, volutpat arcu non, porttitor libero. Duis a ex feugiat dolor vestibulum pellentesque. Nullam viverra tincidunt tortor, in tempor nulla vestibulum tincidunt. Ut tincidunt convallis pretium.
+              Aliquam malesuada rhoncus nunc, sed porta dui efficitur interdum. Nulla facilisi. Sed pellentesque orci tincidunt dolor gravida, et fermentum nibh hendrerit. Aliquam ac fermentum dui, sed mattis mauris. Curabitur nec suscipit ante. Fusce tincidunt eleifend venenatis. Pellentesque iaculis dignissim ligula cursus tempus. Pellentesque eu congue nisl. Nullam lorem felis, venenatis eget purus ut, mattis volutpat nulla. Donec sit amet pulvinar justo, a luctus dolor
+              Nullam id enim ut arcu semper interdum et at tortor. Nunc at magna quam. Mauris consectetur libero mi, eget euismod metus tincidunt a. Aliquam at ultrices ipsum. Sed placerat mattis dui eget viverra. Donec at leo suscipit, vulputate risus sed, lacinia enim. Phasellus id porttitor orci, auctor elementum sem.
+              Nulla volutpat neque sem, ut ornare tortor venenatis et. Donec felis odio, accumsan ac semper interdum, mollis in lectus. Curabitur vel viverra massa. Maecenas dignissim congue dui, a convallis velit suscipit a. Vestibulum massa nisi, consequat et augue ac, molestie facilisis velit. Suspendisse venenatis in urna in viverra. Sed commodo libero metus, eget iaculis nunc consequat in. Vivamus mattis augue mi, vitae maximus leo vestibulum nec. In hac habitasse platea dictumst. Curabitur dignissim pellentesque erat vitae hendrerit.
+              Sed finibus est a nunc convallis, quis egestas lectus cursus. Donec ornare risus nibh, ut ultrices ex auctor eget. Quisque eros mi, congue scelerisque metus id, faucibus tincidunt lorem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec libero mauris, pretium eu volutpat sit amet, sagittis at odio. Nulla feugiat porta massa, eu vestibulum augue varius at. Nam vitae mauris id magna ultrices varius. Curabitur auctor fermentum leo in maximus. Fusce lobortis, risus quis porttitor placerat, mauris nisl viverra tortor, sit amet ullamcorper tortor nibh in enim. Aenean dapibus blandit eros in euismod. Praesent sed egestas nisi. Morbi dignissim mauris at elit venenatis, sit amet bibendum elit facilisis. Sed egestas egestas justo id ultricies. Ut blandit ligula ut rhoncus faucibus.
+            </p>
+          </div>
+        </section>
+        
+        <section>
+          <div class="sectionHeader">
+            <h1>Web Development - React & ThreeJS</h1>
+          </div>
+          <div class="sectionContent">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla rhoncus convallis consectetur. Cras lorem nibh, tincidunt ut molestie ac, tristique nec leo. Suspendisse non laoreet nisi, eu ultrices augue. Morbi vitae orci sagittis, ullamcorper massa sed, commodo lectus. Suspendisse potenti. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer sit amet justo vulputate, volutpat arcu non, porttitor libero. Duis a ex feugiat dolor vestibulum pellentesque. Nullam viverra tincidunt tortor, in tempor nulla vestibulum tincidunt. Ut tincidunt convallis pretium.
+              Aliquam malesuada rhoncus nunc, sed porta dui efficitur interdum. Nulla facilisi. Sed pellentesque orci tincidunt dolor gravida, et fermentum nibh hendrerit. Aliquam ac fermentum dui, sed mattis mauris. Curabitur nec suscipit ante. Fusce tincidunt eleifend venenatis. Pellentesque iaculis dignissim ligula cursus tempus. Pellentesque eu congue nisl. Nullam lorem felis, venenatis eget purus ut, mattis volutpat nulla. Donec sit amet pulvinar justo, a luctus dolor
+              Nullam id enim ut arcu semper interdum et at tortor. Nunc at magna quam. Mauris consectetur libero mi, eget euismod metus tincidunt a. Aliquam at ultrices ipsum. Sed placerat mattis dui eget viverra. Donec at leo suscipit, vulputate risus sed, lacinia enim. Phasellus id porttitor orci, auctor elementum sem.
+              Nulla volutpat neque sem, ut ornare tortor venenatis et. Donec felis odio, accumsan ac semper interdum, mollis in lectus. Curabitur vel viverra massa. Maecenas dignissim congue dui, a convallis velit suscipit a. Vestibulum massa nisi, consequat et augue ac, molestie facilisis velit. Suspendisse venenatis in urna in viverra. Sed commodo libero metus, eget iaculis nunc consequat in. Vivamus mattis augue mi, vitae maximus leo vestibulum nec. In hac habitasse platea dictumst. Curabitur dignissim pellentesque erat vitae hendrerit.
+              Sed finibus est a nunc convallis, quis egestas lectus cursus. Donec ornare risus nibh, ut ultrices ex auctor eget. Quisque eros mi, congue scelerisque metus id, faucibus tincidunt lorem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec libero mauris, pretium eu volutpat sit amet, sagittis at odio. Nulla feugiat porta massa, eu vestibulum augue varius at. Nam vitae mauris id magna ultrices varius. Curabitur auctor fermentum leo in maximus. Fusce lobortis, risus quis porttitor placerat, mauris nisl viverra tortor, sit amet ullamcorper tortor nibh in enim. Aenean dapibus blandit eros in euismod. Praesent sed egestas nisi. Morbi dignissim mauris at elit venenatis, sit amet bibendum elit facilisis. Sed egestas egestas justo id ultricies. Ut blandit ligula ut rhoncus faucibus.
+            </p>
+          </div>
+        </section>
+        
+        <section>
+          <div class="sectionHeader">
+            <h1>Game Development - Unity</h1>
+          </div>
+          <div class="sectionContent">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla rhoncus convallis consectetur. Cras lorem nibh, tincidunt ut molestie ac, tristique nec leo. Suspendisse non laoreet nisi, eu ultrices augue. Morbi vitae orci sagittis, ullamcorper massa sed, commodo lectus. Suspendisse potenti. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer sit amet justo vulputate, volutpat arcu non, porttitor libero. Duis a ex feugiat dolor vestibulum pellentesque. Nullam viverra tincidunt tortor, in tempor nulla vestibulum tincidunt. Ut tincidunt convallis pretium.
+              Aliquam malesuada rhoncus nunc, sed porta dui efficitur interdum. Nulla facilisi. Sed pellentesque orci tincidunt dolor gravida, et fermentum nibh hendrerit. Aliquam ac fermentum dui, sed mattis mauris. Curabitur nec suscipit ante. Fusce tincidunt eleifend venenatis. Pellentesque iaculis dignissim ligula cursus tempus. Pellentesque eu congue nisl. Nullam lorem felis, venenatis eget purus ut, mattis volutpat nulla. Donec sit amet pulvinar justo, a luctus dolor
+              Nullam id enim ut arcu semper interdum et at tortor. Nunc at magna quam. Mauris consectetur libero mi, eget euismod metus tincidunt a. Aliquam at ultrices ipsum. Sed placerat mattis dui eget viverra. Donec at leo suscipit, vulputate risus sed, lacinia enim. Phasellus id porttitor orci, auctor elementum sem.
+              Nulla volutpat neque sem, ut ornare tortor venenatis et. Donec felis odio, accumsan ac semper interdum, mollis in lectus. Curabitur vel viverra massa. Maecenas dignissim congue dui, a convallis velit suscipit a. Vestibulum massa nisi, consequat et augue ac, molestie facilisis velit. Suspendisse venenatis in urna in viverra. Sed commodo libero metus, eget iaculis nunc consequat in. Vivamus mattis augue mi, vitae maximus leo vestibulum nec. In hac habitasse platea dictumst. Curabitur dignissim pellentesque erat vitae hendrerit.
+              Sed finibus est a nunc convallis, quis egestas lectus cursus. Donec ornare risus nibh, ut ultrices ex auctor eget. Quisque eros mi, congue scelerisque metus id, faucibus tincidunt lorem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec libero mauris, pretium eu volutpat sit amet, sagittis at odio. Nulla feugiat porta massa, eu vestibulum augue varius at. Nam vitae mauris id magna ultrices varius. Curabitur auctor fermentum leo in maximus. Fusce lobortis, risus quis porttitor placerat, mauris nisl viverra tortor, sit amet ullamcorper tortor nibh in enim. Aenean dapibus blandit eros in euismod. Praesent sed egestas nisi. Morbi dignissim mauris at elit venenatis, sit amet bibendum elit facilisis. Sed egestas egestas justo id ultricies. Ut blandit ligula ut rhoncus faucibus.
+            </p>
+          </div> 
+        </section>
+        
+        <section>
+          <div class="sectionHeader">
+            <h1>Software Development - VBA Professional</h1>
+          </div>
+          <div class="sectionContent">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla rhoncus convallis consectetur. Cras lorem nibh, tincidunt ut molestie ac, tristique nec leo. Suspendisse non laoreet nisi, eu ultrices augue. Morbi vitae orci sagittis, ullamcorper massa sed, commodo lectus. Suspendisse potenti. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer sit amet justo vulputate, volutpat arcu non, porttitor libero. Duis a ex feugiat dolor vestibulum pellentesque. Nullam viverra tincidunt tortor, in tempor nulla vestibulum tincidunt. Ut tincidunt convallis pretium.
+              Aliquam malesuada rhoncus nunc, sed porta dui efficitur interdum. Nulla facilisi. Sed pellentesque orci tincidunt dolor gravida, et fermentum nibh hendrerit. Aliquam ac fermentum dui, sed mattis mauris. Curabitur nec suscipit ante. Fusce tincidunt eleifend venenatis. Pellentesque iaculis dignissim ligula cursus tempus. Pellentesque eu congue nisl. Nullam lorem felis, venenatis eget purus ut, mattis volutpat nulla. Donec sit amet pulvinar justo, a luctus dolor
+              Nullam id enim ut arcu semper interdum et at tortor. Nunc at magna quam. Mauris consectetur libero mi, eget euismod metus tincidunt a. Aliquam at ultrices ipsum. Sed placerat mattis dui eget viverra. Donec at leo suscipit, vulputate risus sed, lacinia enim. Phasellus id porttitor orci, auctor elementum sem.
+              Nulla volutpat neque sem, ut ornare tortor venenatis et. Donec felis odio, accumsan ac semper interdum, mollis in lectus. Curabitur vel viverra massa. Maecenas dignissim congue dui, a convallis velit suscipit a. Vestibulum massa nisi, consequat et augue ac, molestie facilisis velit. Suspendisse venenatis in urna in viverra. Sed commodo libero metus, eget iaculis nunc consequat in. Vivamus mattis augue mi, vitae maximus leo vestibulum nec. In hac habitasse platea dictumst. Curabitur dignissim pellentesque erat vitae hendrerit.
+              Sed finibus est a nunc convallis, quis egestas lectus cursus. Donec ornare risus nibh, ut ultrices ex auctor eget. Quisque eros mi, congue scelerisque metus id, faucibus tincidunt lorem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec libero mauris, pretium eu volutpat sit amet, sagittis at odio. Nulla feugiat porta massa, eu vestibulum augue varius at. Nam vitae mauris id magna ultrices varius. Curabitur auctor fermentum leo in maximus. Fusce lobortis, risus quis porttitor placerat, mauris nisl viverra tortor, sit amet ullamcorper tortor nibh in enim. Aenean dapibus blandit eros in euismod. Praesent sed egestas nisi. Morbi dignissim mauris at elit venenatis, sit amet bibendum elit facilisis. Sed egestas egestas justo id ultricies. Ut blandit ligula ut rhoncus faucibus.
+            </p>
+          </div>
+        </section>
+        
+        <section>
+          <div class="sectionHeader">
+            <h3>Find me on my socials</h3>
+          </div>
+          <nav class="siteLinks">
+            <ul>
+              <li>
+                <p>LinkedIn</p>
+                <a href="https://www.linkedin.com/in/lee-stevens-dev/">
+                  <img src="images/icon - linkedin.png"></img>
+                </a>
+              </li>
+              <li>
+                <p>GitHub</p>
+                <a href="https://github.com/leesprojects">
+                  <img src="images/icon - github.png"></img> 
+                </a>
+              </li>
+              <li>
+                <p>Leetcode</p>
+                <a href="https://leetcode.com/Zaeroso/">
+                  <img src="images/icon - leetcode.png"></img>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </section>
+
 
       </main>     
-
     </div>
   )
 }
