c9f29dd436c675f97ee4c799fb3d90ecd94b8830
leesprojects <leepeterstevens@gmail.com>
2022-07-17 23:45:36 +0100

    Merge with main

---
+++ src/components/page-links/index.jsx
@@ -0,0 +1,19 @@
+import './index.scss'
+
+const Links = () => {
+    return (
+      <>
+        <section id="navBar">
+            <nav>
+                <ul>
+                    <li><a href="https://www.linkedin.com/in/lee-stevens-dev/">LinkedIn</a></li>
+                    <li><a href="https://github.com/leesprojects">GitHub</a></li>
+                    <li><a href="https://leetcode.com/Zaeroso/">Leetcode</a></li>
+                </ul>
+            </nav>
+        </section>
+      </>
+    )
+}
+
+export default Links
