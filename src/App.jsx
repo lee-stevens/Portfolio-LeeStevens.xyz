eab4d374f8b1cea7937acd7d39d73627aadc3471
leesprojects <leepeterstevens@gmail.com>
2022-07-18 18:09:30 +0100

    page router component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import NavBar   from './components/navbar/' 
-import Home     from './components/page-home/'
-import Unity    from './components/page-unity/'
-import WebDev   from './components/page-webdev'
+import PageRouter from './components/pagerouter/pagerouter'
 import './App.css'
 
 function App() {
   return (
     <div className='App'>
       <NavBar/>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/Unity" element={<Unity/>}/>
-          <Route path="/WebDev" element={<WebDev/>}/>
-        </Routes>
-      </Router>
+      <PageRouter/>
     </div>
   )
 }
--- src/components/pagerouter/pagerouter.jsx
+++ src/components/pagerouter/pagerouter.jsx
@@ -1,17 +1,17 @@
-import { Route, Routes} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Home     from '../page-home'
 import Unity    from '../page-unity'
 import WebDev   from '../page-webdev'
 
 function PageRouter() {
     return (
-        <PageRouter>
+        <Router>
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/Unity" element={<Unity/>}/>
                 <Route path="/WebDev" element={<WebDev/>}/>
             </Routes>
-        </PageRouter>
+        </Router>
     )
   }
   
