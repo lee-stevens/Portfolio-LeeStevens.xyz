72c1df6cd6cb93492f8eaa7d3d5018eab727eebc
leesprojects <leepeterstevens@gmail.com>
2022-07-23 19:42:06 +0100

    Homepage content and style changes

--- src/components/page-home/home.jsx
+++ src/components/page-home/home.jsx
@@ -57,44 +57,33 @@ const Home = () => {
             variants={contentSectionAnimation} 
             transition={{ duration: 2}}>
 
-            <section ref={sectionWebDev}>
-                <div className="headerSection"><h1>Web Development - React & ThreeJS</h1></div>
-                <div className="contentSection">
-                    <div className="tileContainer"> 
-                    <a href="/"><img className="tileItem" id="item1" src="./images/icon - ls.jpg"/></a>
-                    <a href="/Unity-Wildlands"><img className="tileItem" id="item2" src="./images/icon - ls.jpg"/></a>
-                    <a href="/"><img className="tileItem" id="item3"  src="./images/icon - ls.jpg"/></a>
-                </div>
-                </div>
-            </section>
-
-            <div className="sectionDivider"></div>
-            
             <section ref={sectionUnity}>
                 <div className="headerSection"><h1 >Game Development - Unity</h1></div>
                 <div className="contentSection">
-                    <div className="tileContainer"> 
-                        <a href="/"><img className="tileItem" id="item1" src="./images/icon - ls.jpg"/></a>
-                        <a href="/"><img className="tileItem" id="item2" src="./images/icon - ls.jpg"/></a>
-                        <a href="/"><img className="tileItem" id="item3"  src="./images/icon - ls.jpg"/></a>
+                    <div className="tilesContainer">
+                        <div className="tileContainer">
+                            <a href="/Unity-Wildlands"><img className="tileImage" id="item1" src="./images/icon - ls.jpg"/></a>
+                            <div className="tileContent">
+                                <h2>Wildlands</h2>
+                                <p>Skill 1, Skill 2, Skill 3</p>
+                                <p>Wildlands was a submission for my MA module for producing Unity Prototypes. Wildlands was a submission for my MA module for producing Unity Prototypes. Wildlands was a submission for my MA module for producing Unity Prototypes</p>
+                            </div> 
+                        </div>
+                        <div className="tileContainer">
+                            <a href="/"><img className="tileImage" id="item1" src="./images/icon - ls.jpg"/></a>
+                            <div className="tileContent">
+                                <h2>Project Epsilon</h2>
+                                <p>Skill 1, Skill 2, Skill 3</p>
+                                <p>Epsilon is a hobby project. Epsilon is a hobby project Epsilon is a hobby project Epsilon is a hobby project Epsilon is a hobby project Epsilon is a hobby project Epsilon is a hobby project Epsilon is a hobby project</p>
+                            </div> 
+                        </div>
                     </div>
                 </div> 
             </section>
 
             <div className="sectionDivider"></div>
 
-            <section ref={sectionVBA}>
-                <div className="headerSection"><h1>Software Development - VBA</h1></div>
-                <div className="contentSection">
-                    <div className="tileContainer"> 
-                        <a href="/"><img className="tileItem" id="item1" src="./images/icon - ls.jpg"/></a>
-                        <a href="/"><img className="tileItem" id="item2" src="./images/icon - ls.jpg"/></a>
-                        <a href="/"><img className="tileItem" id="item3"  src="./images/icon - ls.jpg"/></a>
-                    </div>
-                </div>
-            </section>
 
-            <div className="sectionDivider"></div>
 
         <footer id="sectionContact">
             <div className="headerSection"><h1>My Socials</h1></div>
