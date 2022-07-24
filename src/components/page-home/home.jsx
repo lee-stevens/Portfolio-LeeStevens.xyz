33be7ee9616016585893402031771ccebd9b8c1a
leesprojects <leepeterstevens@gmail.com>
2022-07-24 16:54:16 +0100

    Layout changes

--- src/components/page-home/home.jsx
+++ src/components/page-home/home.jsx
@@ -7,7 +7,7 @@ import { useRef } from 'react'
 const Home = () => {
     const headerSection = useRef(null);
     const sectionAbout = useRef(null);
-    const sectionUnity = useRef(null);
+    const sectionGameDev = useRef(null);
     const sectionWebDev = useRef(null);
     const sectionVBA = useRef(null);
     const sectionContact = useRef(null);
@@ -28,7 +28,7 @@ const Home = () => {
                 <button onClick={() => scrollToSection(headerSection)}>Home</button>
                 <button onClick={() => scrollToSection(sectionAbout)}>About Me</button>
                 <button onClick={() => scrollToSection(sectionWebDev)}>Web Dev</button>
-                <button onClick={() => scrollToSection(sectionUnity)}>Game Dev</button>
+                <button onClick={() => scrollToSection(sectionGameDev)}>Game Dev</button>
                 <button onClick={() => scrollToSection(sectionVBA)}>VBA</button>
                 <button onClick={() => scrollToSection(sectionContact)}>Contact</button>
                 <a href="/"><button>Reload</button></a>
@@ -45,7 +45,8 @@ const Home = () => {
             <div className="headerImage">
                 <img src="/images/background-b.jpg"></img>
                 <div className="centeredOverlay">
-                    <p>Lee Stevens</p>
+                    <h1>Lee Stevens</h1>
+                    <p>Scroll down or use the navigation bar.</p>
                 </div>
             </div>
         </section>
@@ -66,12 +67,17 @@ const Home = () => {
                         <div className="tileContent">
                             <h2>Portfolio - ReactJS</h2>
                             <ul>
+                                <li>Front-End</li>
                                 <li>ReactJS</li>
                                 <li>Framer-Motion</li>
                                 <li>SCSS</li>
                                 <li>Routers</li>
                             </ul>
-                            <p>Wildlands was a submission for my MA module for producing Unity Prototypes. Wildlands was a submission for my MA module for producing Unity Prototypes. Wildlands was a submission for my MA module for producing Unity Prototypes</p>
+                            <p>
+                                This website portfolio was my first project in the world of Web Development. <br/>
+                                I learned the basics of HTML and CSS and soon after found ReactJS.<br/>
+                                With a background in C#, I loved the syntax of ReactJS especially it's OOP features<br/>
+                            </p>
                         </div> 
                     </div>
                     <div className="tileContainer">
@@ -79,13 +85,17 @@ const Home = () => {
                         <div className="tileContent">
                             <h2>Portfolio - Three.js</h2>
                             <ul>
+                                <li>Front-End</li>
                                 <li>Three.js</li>
                                 <li>ReactJS</li>
                                 <li>React-Three-Fiber</li>
-                                <li>Framer-Motion</li>
-                                <li>SCSS</li>
                             </ul>
-                            <p>Epsilon is a hobby project. Epsilon is a hobby project Epsilon is a hobby project Epsilon is a hobby project Epsilon is a hobby project Epsilon is a hobby project Epsilon is a hobby project Epsilon is a hobby project</p>
+                            <p>
+                                After developing this portfolio in ReactJS I wanted to try something completely different<br/>
+                                I found that you could make 3D interactive websites using ThreeJS and that you can
+                                integrate ReactJS using React-Three-Fiber. I knew
+                                I was right at home since it was so similar to Unity Game Development<br/>
+                            </p>
                         </div> 
                     </div>
                 </div>
@@ -94,8 +104,8 @@ const Home = () => {
 
 
 
-        <section className="wholePage" ref={sectionUnity}>
-            <div className="headerSection"><h1 >Game Development - Unity</h1></div>
+        <section className="wholePage" ref={sectionGameDev}>
+            <div className="headerSection"><h1>Game Development</h1></div>
             
             <div className="sectionContent">
                 <div className="tilesContainer">
@@ -104,12 +114,21 @@ const Home = () => {
                         <div className="tileContent">
                             <h2>Wildlands</h2>
                             <ul>
-                                <li>Skill 1</li>
-                                <li>Skill 2</li>
-                                <li>Skill 3</li>
-                                <li>Skill 4</li>
+                                <li>Unity</li>
+                                <li>C#</li>
+                                <li>Python</li>                                
                             </ul>
-                            <p>Wildlands was a submission for my MA module for producing Unity Prototypes. Wildlands was a submission for my MA module for producing Unity Prototypes. Wildlands was a submission for my MA module for producing Unity Prototypes</p>
+                            <p>
+                                Wildlands was a submission for my MA module <i>Prototyping</i> where I worked in a team of 2 to produce 
+                                prototypes at 4 stages.<br/>
+                            </p>
+                            <ol>
+                                    <li>Paper (Conceptual)</li>
+                                    <li>2D Clickteam</li>
+                                    <li>3D Unity MVP</li>
+                                    <li>3D Unity Demo</li>
+                                </ol>
+                            
                         </div> 
                     </div>
                     <div className="tileContainer">
@@ -117,12 +136,57 @@ const Home = () => {
                         <div className="tileContent">
                             <h2>Project Epsilon</h2>
                             <ul>
-                                <li>Skill 1</li>
-                                <li>Skill 2</li>
-                                <li>Skill 3</li>
-                                <li>Skill 4</li>
+                                <li>Unity</li>
+                                <li>C#</li>
+                                <li>Python</li>
+                                <li>Machine Learning</li>
+                                <li>3D Modelling</li>
                             </ul>
-                            <p>Epsilon is a hobby project. Epsilon is a hobby project Epsilon is a hobby project Epsilon is a hobby project Epsilon is a hobby project Epsilon is a hobby project Epsilon is a hobby project Epsilon is a hobby project</p>
+                            <p>
+                                Epsilon is a hobby project.<br/>
+                            </p>
+                        </div> 
+                    </div>
+                </div>
+            </div> 
+        </section>
+
+
+
+        <section className="wholePage" ref={sectionVBA}>
+            <div className="headerSection"><h1 >VBA - Professional Work</h1></div>
+            
+            <div className="sectionContent">
+                <div className="tilesContainer">
+                    <div className="tileContainer">
+                        <a href="/Unity-Wildlands"><img className="tileImage" id="item1" src="./images/icon - ls.jpg"/></a>
+                        <div className="tileContent">
+                            <h2>File Automation</h2>
+                            <ul>
+                                <li>VBA</li>
+                                <li>Excel</li>
+                                <li>Microsoft 365</li>
+                                <li>Automation</li>                                
+                            </ul>
+                            <p>
+                                <br/>
+                            </p>
+                            
+                        </div> 
+                    </div>
+                    <div className="tileContainer">
+                        <a href="/"><img className="tileImage" id="item1" src="./images/icon - ls.jpg"/></a>
+                        <div className="tileContent">
+                            <h2>Invoice Generators</h2>
+                            <ul>
+                                <li>VBA</li>
+                                <li>Excel</li>
+                                <li>Word</li>
+                                <li>Automation</li>
+                            </ul>
+                            <p>
+                                <br/>
+                            </p>
                         </div> 
                     </div>
                 </div>
